80ff5873c8e09ebc4ea01054373283d0078065f2
sepehr <sepehrsanaeiazad@gmail.com>
2025-03-12 18:44:33 +0330

    added searching to profiles API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers, models, permissions
@@ -101,3 +100,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfille,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
