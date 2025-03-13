cdc7e2cc13ccfc25cabc3758ebb5c5e414862f00
sepehr <sepehrsanaeiazad@gmail.com>
2025-03-13 19:55:39 +0330

    fixed some bugs in setup file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,26 +2,25 @@
 
 set -e
 
+# TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/sepehr-sanaei/profile-rest-api.git'
+
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-# Install Python 3.11 from deadsnakes PPA
+# Set Ubuntu Language
+locale-gen en_GB.UTF-8
+
+# Install Python, SQLite and pip
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:deadsnakes/ppa
-apt-get update
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
-# Install Python 3.11 and dependencies
-apt-get install -y python3.11 python3.11-dev python3.11-venv sqlite3 supervisor nginx git
-
-# Create virtual environment with Python 3.11
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
-python3.11 -m venv $PROJECT_BASE_PATH/env
 
-# Install requirements
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+python3 -m venv $PROJECT_BASE_PATH/env
+
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
