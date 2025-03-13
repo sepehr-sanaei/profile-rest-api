489bed002fe917f9f54214433ea2dce4f7777ebe
sepehr <sepehrsanaeiazad@gmail.com>
2025-03-13 19:47:48 +0330

    fixed some bugs in setup file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,25 +2,21 @@
 
 set -e
 
-# TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/sepehr-sanaei/profile-rest-api.git'
-
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-# Set Ubuntu Language
-locale-gen en_GB.UTF-8
-
-# Install Python, SQLite and pip
+# Install Python 3.11 and dependencies
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
+apt-get install -y python3.11 python3.11-dev python3.11-venv sqlite3 supervisor nginx git
 
+# Create virtual environment with Python 3.11
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+python3.11 -m venv $PROJECT_BASE_PATH/env
 
-python3 -m venv $PROJECT_BASE_PATH/env
-
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Install requirements (remove explicit uwsgi version if in requirements.txt)
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
