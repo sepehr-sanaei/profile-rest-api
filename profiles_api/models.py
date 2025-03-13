4cf1925a72a53368269553f955693fee4e29066b
sepehr <sepehrsanaeiazad@gmail.com>
2025-03-13 13:28:11 +0330

    Added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
@@ -50,3 +55,14 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Returning string representation of user"""
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Update user profile status"""
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,12 @@ class UpdateOwnProfille(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow user to update own Status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Checks user is updating their own Status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
